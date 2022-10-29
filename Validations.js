0d9466df1bbaf7b4d40b6b00c62ae196f619c13d
Christopher Shaw <silvertn350@yahoo.com>
2022-10-29 11:00:55 -0500

    1st commit

---
+++ Validations.js
@@ -0,0 +1,28 @@
+Validations Resource: https://mongoosejs.com/docs/validation.html
+
+
+// Example validation for User 
+// first name, last name and email are required
+
+
+const UserSchema = new mongoose.Schema(
+    {
+        first_name:{
+            type: String,
+            required: [true, 'First name is required'],
+            minlength: [6, 'First name must be at least 6 characters long']
+        },
+        last_name:{
+            type: String,
+            required: [true, 'Last name is required'],
+            minlength: [6, 'Last name must be at least 6 characters long']
+        },
+        age:{
+            type: Number,
+            min: [18, "You must be 18 or older to register"],
+            max: [150, "You must be at most 149 years old to register"]
+        },
+        email: { type: String, required: [true, "email is required"]}
+    },
+    {timestamps: true}
+);
